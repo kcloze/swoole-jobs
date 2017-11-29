d55979e87796cf74006205a098eb443630affbed
lixiaopei <lixiaopei@yaochufa.com>
2017-11-29 17:27:15 +0800

    批量格式代码

--- src/JobObject.php
+++ src/JobObject.php
@@ -1,8 +1,11 @@
 <?php
-/*
-job实体
 
-*/
+/*
+ * This file is part of PHP CS Fixer.
+ * (c) kcloze <pei.greet@qq.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Kcloze\Jobs;
 
