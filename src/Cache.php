f6c27fb1d065df5a49851cff49703b124e7617f7
kcloze <460309735@qq.com>
2017-12-18 22:39:23 +0800

    格式化代码

--- src/Cache.php
+++ src/Cache.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of PHP CS Fixer.
- * (c) php-team@yaochufa <php-team@yaochufa.com>
+ * (c) kcloze <pei.greet@qq.com>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
