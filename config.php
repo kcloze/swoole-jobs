e778162e960fb04027c2bd31bb2b85061399c8ee
kcloze <460309735@qq.com>
2018-04-25 18:42:59 +0800

    去掉配置

--- config.php
+++ config.php
@@ -17,10 +17,9 @@ return $config = [
     'pidPath'           => __DIR__ . '/log',
     'sleep'             => 2, // 队列没消息时，暂停秒数
     'queueMaxNum'       => 10, // 队列达到一定长度，启动动态子进程个数发和送消息提醒
-    'maxPopNum'         => 100, // 子进程启动后每个循环最多取多少个job
     'excuteTime'        => 3600, // 子进程最长执行时间，防止内存泄漏
-    'queueTickTimer'    =>1000 * 15,//一定时间间隔（毫秒）检查队列长度;默认10秒钟
-    'messageTickTimer'  =>1000 * 180, //一定时间间隔（毫秒）发送消息提醒;默认3分钟
+    'queueTickTimer'    => 1000 * 15, //一定时间间隔（毫秒）检查队列长度;默认10秒钟
+    'messageTickTimer'  => 1000 * 180, //一定时间间隔（毫秒）发送消息提醒;默认3分钟
     'processName'       => ':swooleTopicQueue', // 设置进程名, 方便管理, 默认值 swooleTopicQueue
     //job任务相关
     'job'         => [
@@ -47,17 +46,14 @@ return $config = [
         //     'vhost'         => 'php',
         //     'exchange'      => 'php.amqp.ext',
         // ],
-
    ],
    //框架类型及装载类
    'framework' => [
        //可以自定义，但是该类必须继承\Kcloze\Jobs\Action\BaseAction
        'class'=> '\Kcloze\Jobs\Action\SwooleJobsAction',
-
    ],
    'message'=> [
        'class'  => '\Kcloze\Jobs\Message\DingMessage',
        'token'  => '***your-dingding-token***',
    ],
-
 ];
